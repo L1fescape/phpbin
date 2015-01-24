e53ec55cdc7f9f8d528b737763aadd77dbc2f900
Andrew Kennedy <akennedy2292@gmail.com>
2015-01-23 16:46:29 -0800

    '<?php ?>' is now optional. update examples to not inculed '<?php ?>'. add for loop example.

--- main.js
+++ main.js
@@ -3,13 +3,17 @@ var _ = require('underscore');
 var $ = require('jquery');
 
 var scripts = {
-  hello : "<?php echo('Hello world!'); ?>",
-  printf : "<?php printf('php is number %d', '1'); ?>"
+  hello : "echo('Hello world!');",
+  printf : "printf('php is number %d', '1');",
+  loop : "for ($i = 1; $i <= 10; $i++) { \necho $i; \n}"
 };
 
 $(document).ready(function(){
   $('#eval').click(function(){
     var source = $('#source').val();
+    if (source.substring(0, 6) !== '<?php '){
+      source = '<?php ' + source + ' ?>';
+    }
     var path = window.location.pathname;
     var options = {
       SERVER: {
